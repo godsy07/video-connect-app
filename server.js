f9ed92ac1eb88b0d1803ff72d9b8d094e4b6af68
unknown <godsonemmanuel07@gmail.com>
2021-11-26 20:03:30 +0530

    Added basic styling for the homepage

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
